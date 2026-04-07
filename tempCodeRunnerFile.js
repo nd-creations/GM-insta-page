00833d449014302d6b4c70b83078376e951d070f
Naveen N D <naveendevihosur107@gmail.com>
2026-04-07 20:22:23 +0530

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-// Serve uploaded media
-app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
-// Routes
-app.use('/auth', require('./routes/authRoutes'));
-app.use('/posts', require('./routes/postRoutes'));
-app.use('/comments', require('./routes/commentRoutes'));
-app.use('/chat', require('./routes/chatRoutes'));
