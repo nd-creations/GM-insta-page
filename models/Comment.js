36433b2933b064fbc65b1fdbce805b7dc3a04610
Naveen N D <naveendevihosur107@gmail.com>
2026-04-09 19:06:21 +0530

    Refactor Comment schema with new field names

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
-  postId:      { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-  userId:      { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  commentText: { type: String, required: true }
+const CommentSchema = new mongoose.Schema({
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  text:   { type: String, required: true }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Comment', commentSchema);
+module.exports = mongoose.model('Comment', CommentSchema);
